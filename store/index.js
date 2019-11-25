5dbb5dcdf431f38fb579fb7da769f4dbe00e13ae
Sarah Trefethen <49411879+sarahtrefethen@users.noreply.github.com>
2019-11-24 21:07:30 -0500

    set up a redux skeleton

--- store/index.js
+++ store/index.js
@@ -1,1 +1,12 @@
-import 
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import currentContentReducer from './currentContent';
+import contentListReducer from './contentList';
+
+const logger = createLogger();
+
+const reducers = combineReducers(currentContentReducer, contentListReducer);
+
+export default createStore(reducers, applyMiddleware(thunkMiddleware, logger));
